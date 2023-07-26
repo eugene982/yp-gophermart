f324a7f259c27562eb36b5142cd79e27cae3555e
eugene982 <eugene.spnr@mail.ru>
2023-07-26 20:35:25 +0300

    правки ручек

---
+++ internal/services/database/database.go
@@ -0,0 +1,37 @@
+package database
+
+//go:generate go run github.com/vektra/mockery/v2@v2.32.0 --name=mockdb
+
+import (
+	"context"
+	"errors"
+
+	"github.com/eugene982/yp-gophermart/internal/model"
+)
+
+var (
+	ErrWriteConflict = errors.New("data exists")
+	ErrNoContent     = errors.New("no content")
+	ErrDBNotInit     = errors.New("database not initialize")
+)
+
+// Интерфейс для хранилища данных
+type Database interface {
+	Close() error
+	Ping(context.Context) error
+
+	WriteUser(ctx context.Context, data model.UserInfo) error
+	ReadUser(ctx context.Context, userID string) (model.UserInfo, error)
+
+	WriteNewOrder(ctx context.Context, userID string, order int64) error
+	ReadOrders(ctx context.Context, userID string, orders ...int64) ([]model.OrderInfo, error)
+
+	WriteWithdraw(ctx context.Context, userID string, order int64, sum int) error
+	ReadWithdraws(ctx context.Context, userID string) ([]model.OperationsInfo, error)
+
+	ReadAccruals(ctx context.Context, userID string) ([]model.OperationsInfo, error)
+
+	ReadBalance(ctx context.Context, userID string) (model.BalanceInfo, error)
+	ReadOrdersWithStatus(ctx context.Context, status []string, limit int) ([]model.OrderInfo, error)
+	UpdateOrderAccrual(ctx context.Context, order model.OrderInfo, accrual int) error
+}
